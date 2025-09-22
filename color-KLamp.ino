16306fbf1c0a44452eac18d45032d855b8cee357
Alex <88129688+Snickser@users.noreply.github.com>
2025-09-22 12:40:58 +0300

    Add files via upload

--- color-KLamp.ino
+++ color-KLamp.ino
@@ -22,6 +22,7 @@ unsigned int modeR = 180;     // cyclical change of modes in seconds (for first 
 byte modeL = 1;
 float cLim = 200;    // color for 1/random() 3,6,12,24,48,96,192,384,768
 unsigned int rainbow = 12;
+float colorST = 0.5;
 
 ////
 // <
@@ -68,24 +69,27 @@ void setup() {
   attachInterrupt(digitalPinToInterrupt(S1), isr, CHANGE);  // D2
   attachInterrupt(digitalPinToInterrupt(S2), isr, CHANGE);  // D3
 
+  EEPROM.get(4, colorST);
+  if (isnan(colorST)) colorST = 0.5;
+
   EEPROM.get(1, brC);
   EEPROM.get(0, Mode);
 
   Br = Brightness(brC);
 
-//Mode=5;
-/*
-  while(1){
-    for(byte i=0;i<PixelCount;i++){
-      RgbColor color = HsbColor(1-i/192., 1, Br);
-//      RgbColor color = RgbColor(255-i,i,0);
-       strip.SetPixelColor(i, color);
-       Serial.println(1-i/192.,6);
+  //Mode=5;
+  /*
+    while(1){
+      for(byte i=0;i<PixelCount;i++){
+        RgbColor color = HsbColor(1-i/192., 1, Br);
+    //      RgbColor color = RgbColor(255-i,i,0);
+         strip.SetPixelColor(i, color);
+         Serial.println(1-i/192.,6);
+      }
+        strip.Show();
+        delay(10000);
     }
-      strip.Show();
-      delay(10000);
-  }
-*/
+  */
 
 }
 
@@ -95,7 +99,7 @@ void isr() {
 
 byte bb;
 byte bMode;
-unsigned long timeA,timeB,timeL;
+unsigned long timeA, timeB, timeL;
 unsigned int cs;
 unsigned int nRB, nDL;
 
@@ -112,6 +116,8 @@ void loop() {
     timeA = tNow;
     cs = 260;
     switch (Mode) {
+      case 9:
+        break;
       case 8:
         blank(0);
         break;
@@ -136,7 +142,7 @@ void loop() {
         m3(0);
         break;
       case 1:
-        cs = random(150,400);
+        cs = random(150, 400);
         m0();
         break;
       default:
@@ -146,7 +152,7 @@ void loop() {
     strip.Show();
 
   }
-   
+
 
   // blink
   if ( tNow - timeB > 1000 ) {
@@ -160,19 +166,19 @@ void loop() {
       bb = 1;
     }
 
-//     Serial.print(modeL); Serial.print(" "); Serial.print(Mode); Serial.print(" "); Serial.println(modeR-(tNow-timeL)/1000);
+    //     Serial.print(modeL); Serial.print(" "); Serial.print(Mode); Serial.print(" "); Serial.println(modeR-(tNow-timeL)/1000);
   } //
 
   // random Mode
-  if ( (tNow - timeL) / 1000 > modeR && Mode!=0 && Mode!=8) {
+  if ( (tNow - timeL) / 1000 > modeR && Mode != 0 && Mode != 8 && Mode != 9) {
     timeL = tNow;
-    modeR = random(29,300);
+    modeR = random(29, 300);
     modeL = Mode;
-    while( Mode == modeL ) Mode = random(1,8);
+    while ( Mode == modeL ) Mode = random(1, 8);
     rainbow = random(6, cLim);
-    nRB=1;
+    nRB = 1;
     Serial.print(Mode); Serial.print(" "); Serial.println(modeR);
-  } 
+  }
 
 } //end loop()
 
@@ -182,23 +188,46 @@ void loop() {
 
 void encoder() {
   if (enc.isRight()) {
-    brC--;
-    if (brC < 1) brC = 1;
-    Br = Brightness(brC);
-    EEPROM.put(1, brC);
+    if (Mode == 9) {
+      colorST += 1. / 256;
+      if (colorST > 1) colorST = 0;
+      setcolor(colorST);
+    } else {
+      brC--;
+      if (brC < 1) brC = 1;
+      Br = Brightness(brC);
+      EEPROM.put(1, brC);
+    }
   } else if (enc.isLeft()) {
-    brC++;
-    Br = Brightness(brC);
-    EEPROM.put(1, brC);
+    if (Mode == 9) {
+      colorST -= 1. / 256;
+      if (colorST < 0) colorST = 1;
+      setcolor(colorST);
+    } else {
+      brC++;
+      Br = Brightness(brC);
+      EEPROM.put(1, brC);
+    }
   } else if (enc.isClick()) {
     Mode++;
-    if (Mode > 8){ Mode=0; }
-//    timeL = millis();    
-    nRB=1;
+    if (Mode > 8) {
+      Mode = 0;
+    }
+    //    timeL = millis();
+    nRB = 1;
     EEPROM.put(0, Mode);
+    EEPROM.put(4, colorST);
+  } else if (enc.isHold()) {
+    Mode = 9;
+    strip.ClearTo(0);
   }
 }
 
+void setcolor(float x) {
+  RgbColor color = HsbColor(x, 1, Br);
+  strip.ClearTo(color); // вся лента
+  strip.Show();
+}
 
 void blank(byte x) {
   for (int i = 0; i < PixelCount; i++) {
@@ -208,120 +237,120 @@ void blank(byte x) {
 }
 
 void m0() {
-    for(byte i=0;i<PixelCount;i++){
-     if(random(3)==1){
-      float RB = random(cLim+1) / cLim;
+  for (byte i = 0; i < PixelCount; i++) {
+    if (random(3) == 1) {
+      float RB = random(cLim + 1) / cLim;
       RgbColor color = HsbColor(RB, 1, Br);
       strip.SetPixelColor(i, color);
-     }
     }
+  }
 }
 
 // white
 void m2() {
-//    HsbColor cW = RgbColor(255,144,64);
-    for(byte i=0;i<PixelCount;i++){
-//      RgbColor color = HsbColor(cW.H, cW.S, Br);
-      RgbwColor color = RgbwColor(Br*255);
-      strip.SetPixelColor(i, color);
-    }
+  //    HsbColor cW = RgbColor(255,144,64);
+  for (byte i = 0; i < PixelCount; i++) {
+    //      RgbColor color = HsbColor(cW.H, cW.S, Br);
+    RgbwColor color = RgbwColor(Br * 255);
+    strip.SetPixelColor(i, color);
+  }
 }
 
-float lastRB,nextRB,getRB;
-byte sW=0;
-void m3(byte x){
-    if(nRB==0 && nDL++<300){
-      return;
+float lastRB, nextRB, getRB;
+byte sW = 0;
+void m3(byte x) {
+  if (nRB == 0 && nDL++ < 300) {
+    return;
+  }
+  if (nRB++ >= 50) {
+    nRB = 0;
+    nDL = 0;
+    if (x) {
+      if (++sW > 1)sW = 0;
+      HsbColor px = RgbColor(strip.GetPixelColor(sW));
+      getRB = px.H;
+    } else {
+      sW = 0;
+      getRB = lastRB;
     }
-    if(nRB++ >= 50){
-      nRB=0;
-      nDL=0;
-      if(x){
-        if(++sW>1)sW=0;
-        HsbColor px = RgbColor(strip.GetPixelColor(sW));
-        getRB = px.H;
-      } else {
-        sW=0;
-        getRB = lastRB;
-      }
-      byte c=1;
-      while(abs(nextRB-getRB)<0.08 || abs(nextRB-lastRB)<0.08 || c==1){
-        nextRB = random(cLim+1) / cLim;
-        c=0;
-      }
-      lastRB = nextRB;
+    byte c = 1;
+    while (abs(nextRB - getRB) < 0.08 || abs(nextRB - lastRB) < 0.08 || c == 1) {
+      nextRB = random(cLim + 1) / cLim;
+      c = 0;
     }
-    float nn = Map(nRB, 0, 50, 0, 1);
-    RgbColor res = RgbColor::LinearBlend(HsbColor(getRB, 1, Br), HsbColor(nextRB, 1, Br), nn);
-    for(byte i=0;i<PixelCount;i+=x+1){
-        strip.SetPixelColor(i+sW, res);
-    }
+    lastRB = nextRB;
+  }
+  float nn = Map(nRB, 0, 50, 0, 1);
+  RgbColor res = RgbColor::LinearBlend(HsbColor(getRB, 1, Br), HsbColor(nextRB, 1, Br), nn);
+  for (byte i = 0; i < PixelCount; i += x + 1) {
+    strip.SetPixelColor(i + sW, res);
+  }
 }
 
-void m4(byte x){
+void m4(byte x) {
   float RB;
-  if(x==0){
+  if (x == 0) {
     strip.ShiftRight(1);
   } else {
     strip.ShiftLeft(1);
   }
-  if(nRB==1){
-    nRB=0;
-    for(byte i=0;i<PixelCount;i++){
-      RB = random(cLim+1) / cLim;
+  if (nRB == 1) {
+    nRB = 0;
+    for (byte i = 0; i < PixelCount; i++) {
+      RB = random(cLim + 1) / cLim;
       RgbColor color = HsbColor(RB, 1, Br);
       strip.SetPixelColor(i, color);
     }
   } else {
-    RB = random(cLim+1) / cLim;
+    RB = random(cLim + 1) / cLim;
     RgbColor color = HsbColor(RB, 1, Br);
-    strip.SetPixelColor((PixelCount-1)*x, color);
+    strip.SetPixelColor((PixelCount - 1)*x, color);
   }
 }
 
-int stp=-1;
+int stp = -1;
 void m5(byte x) {
 
-  if(nRB==1){
-    nRB=0;
-    for(byte i=0;i<PixelCount;i++){
-      stp+=1;
-      if(stp>rainbow) stp=0;
+  if (nRB == 1) {
+    nRB = 0;
+    for (byte i = 0; i < PixelCount; i++) {
+      stp += 1;
+      if (stp > rainbow) stp = 0;
       float nn = Map(stp, 0, rainbow, 1, 0);
       RgbColor color = HsbColor(nn, 1, Br);
       strip.SetPixelColor(i, color);
     }
   }
-    
-    stp+=1;
-    if(stp>rainbow) stp=0; 
-    float nn = Map(stp, 0, rainbow, 1, 0);
- Serial.println(nn,3);
-    RgbColor res = HsbColor(nn, 1, Br);
-    if(x==0){
-      strip.ShiftRight(1);
-    } else {
-      strip.ShiftLeft(1);
-    }
-    strip.SetPixelColor((PixelCount-1)*x, res);
+
+  stp += 1;
+  if (stp > rainbow) stp = 0;
+  float nn = Map(stp, 0, rainbow, 1, 0);
+  Serial.println(nn, 3);
+  RgbColor res = HsbColor(nn, 1, Br);
+  if (x == 0) {
+    strip.ShiftRight(1);
+  } else {
+    strip.ShiftLeft(1);
+  }
+  strip.SetPixelColor((PixelCount - 1)*x, res);
 }
 
-byte mtr[][2]={ {1,4},{6,12},{14,16},{19,24},{27,29},{33,35},{39,49} };
+byte mtr[][2] = { {1, 4}, {6, 12}, {14, 16}, {19, 24}, {27, 29}, {33, 35}, {39, 49} };
 byte lsW;
-void m6(){
-    if(nRB==0 && nDL++<500){
-      return;
-    }
-    if(nRB==0) blank(0);
-    float nn = Map(nRB, 0, 50, 0, Br);
-    RgbwColor res = RgbwColor(nn*255);
-    for(byte i=mtr[sW][0]-1;i<=mtr[sW][1]-1;i++){
-        strip.SetPixelColor(i, res);
-    }
-    if(nRB++ >= 50){
-      nRB=0;
-      nDL=0;
-      while(lsW == sW) sW=random(7);
-      lsW=sW;
-    }
+void m6() {
+  if (nRB == 0 && nDL++ < 500) {
+    return;
+  }
+  if (nRB == 0) blank(0);
+  float nn = Map(nRB, 0, 50, 0, Br);
+  RgbwColor res = RgbwColor(nn * 255);
+  for (byte i = mtr[sW][0] - 1; i <= mtr[sW][1] - 1; i++) {
+    strip.SetPixelColor(i, res);
+  }
+  if (nRB++ >= 50) {
+    nRB = 0;
+    nDL = 0;
+    while (lsW == sW) sW = random(7);
+    lsW = sW;
+  }
 }
